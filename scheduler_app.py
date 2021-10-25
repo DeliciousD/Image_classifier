110ad7df712d40fecf2be3452aca376baf767396
daniel <danielpwhite1993.dw@gmail.com>
2021-10-25 15:13:18 +0100

    more tests
    -D

--- scheduler_app.py
+++ scheduler_app.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
 """
 # Here's a pointless slider
-
 """
 
+@st.cache
+def squared(x):
+    return x * x
+
 x = st.slider('x')
-st.write(x, 'squared is', x * x)
+st.write(x, 'squared is', squared(x))
 
 """
 # Next is a pointless dataframe
@@ -19,7 +23,23 @@ df = pd.read_csv('DB.csv')
 st.dataframe(df)
 
 """
+# A pointless plot to go with
+"""
+
+fig_1 = plt.figure(figsize=(9, 5))
+plt.plot(df['Col1'], df['Col2'])
+st.pyplot(fig_1)
+
+
+"""
 Everything is better with Rick, or is it?
 """
 
 st.image('rick.jpg')
+
+filename = st.text_input(label='Enter a file path:', value='C:/Users/danie/Documents/DW/Scheduler/text_simple.txt')
+try:
+    with open(filename) as input:
+        st.text(input.read())
+except FileNotFoundError:
+    st.error('File not found.')

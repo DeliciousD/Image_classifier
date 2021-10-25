75a40bbe5baecd650c9e5cf60b01140c211cba1c
daniel <danielpwhite1993.dw@gmail.com>
2021-10-25 14:59:06 +0100

    Added link to readme and basic functions to app
    -D

--- scheduler_app.py
+++ scheduler_app.py
@@ -1,4 +1,25 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+
+"""
+# Here's a pointless slider
+
+"""
 
 x = st.slider('x')
 st.write(x, 'squared is', x * x)
+
+"""
+# Next is a pointless dataframe
+"""
+
+df = pd.read_csv('DB.csv')
+
+st.dataframe(df)
+
+"""
+Everything is better with Rick, or is it?
+"""
+
+st.image('rick.jpg')
